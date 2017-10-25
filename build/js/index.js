27063f4be4ea9d97ece886e55b6fcfc944ff710d
hooper-hc <hucheng@baidu.com>
2017-10-25 11:35:22 +0800

    first blood

---
+++ build/js/index.js
@@ -0,0 +1,33 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(1);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(2);
+
+__webpack_require__(3);
+
+var root = document.querySelector('.app-welcome');
+root.innerHTML = 'Welcome to Dawn';
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+],[0]);
+//# sourceMappingURL=index.js.map
